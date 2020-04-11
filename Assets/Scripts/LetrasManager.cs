df03674ca99a48acd67c9b57d4d628521eaac202
Yaiza <yaizacastillo@users.noreply.github.com>
2020-04-12 00:40:50 +0200

    Modo letras mas o menoss

---
+++ Assets/Scripts/LetrasManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetrasManager : MonoBehaviour
+{
+    public Text outlineText;
+    public Text topText;
+    public Text centerText;
+    public Text bottomText;
+
+    char[] abcArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    string[] parametersArray = {"famoso", "comida", "país", "objeto", "marca", "animal", "bebida alcohólica", "discoteca"};
+
+    void Start()
+    {
+        RandomLetter();
+        RandomParameters();
+    }
+
+    void RandomLetter()
+    {
+        int random = Random.Range(0, abcArray.Length);
+
+        outlineText.text = abcArray[random].ToString();
+    }
+
+    void RandomParameters()
+    {
+        int r1 = Random.Range(0, parametersArray.Length);
+        topText.text = parametersArray[r1].ToString();
+
+        int r2 = Random.Range(0, parametersArray.Length);
+        while(r1==r2)
+        {
+            r2 = Random.Range(0, parametersArray.Length);
+        }
+        centerText.text = parametersArray[r2].ToString();
+
+        int r3 = Random.Range(0, parametersArray.Length);
+        while (r1 == r3 || r2 == r3)
+        {
+            r3 = Random.Range(0, parametersArray.Length);
+        }
+        bottomText.text = parametersArray[r3].ToString();
+    }
+
+    public void OnScreenTap()
+    {
+        RandomLetter();
+        RandomParameters();
+    }
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    bool timeEnded = false;
+    public float maxTime = 5.0f;
+    float counterTime = 0.0f;
+    public Text topText;
+    public Text centerText;
+    public Text bottomText;
+    public Text counterText;
+    public Text outlineText;
+    public Image outlineTextImage;
+
+    void Start()
+    {
+        counterTime = maxTime;
+
+        if (outlineTextImage)
+        {
+            outlineTextImage.enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        if (counterTime <= 0.1f)
+        {
+            timeEnded = true;
+            outlineTextImage.enabled = false;
+            outlineText.text = "";
+            topText.text = "";
+            centerText.text = "¡TIEMPO!";
+            bottomText.text = "";
+            
+            if (outlineTextImage)
+            {
+                outlineTextImage.enabled = false;
+            }
+            Handheld.Vibrate();
+        }
+
+        else
+            counterTime -= Time.deltaTime;
+
+        counterText.text = Mathf.FloorToInt(counterTime).ToString();
+    }
+
+    public void OnScreenTap()
+    {
+        counterTime = maxTime;
+        timeEnded = false;
+        if (outlineTextImage)
+        {
+            outlineTextImage.enabled = true;
+        }
+    }
+}
