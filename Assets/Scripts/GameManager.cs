3e262df844518f329d5881207be0f1144e7b7b92
Yaiza <yaizacastillo@users.noreply.github.com>
2020-09-18 13:11:10 +0200

    Funcion boton rating

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -175,6 +175,11 @@ public class GameManager : MonoBehaviour
         anyCanvasActive = !anyCanvasActive;
     }
 
+    public void OpenStore()
+    {
+        Application.OpenURL("market://details?id=" + Application.identifier);
+    }
+
     //--------------------------------------------Unity Ads-----------------------------------------
 
     public void ShowInterstitialAd()
