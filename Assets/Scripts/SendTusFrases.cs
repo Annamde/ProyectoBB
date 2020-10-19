87238d54b3fc0d46d77b93e81dd9d8cf48f7db9e
annamde <annadiaz553@gmail.com>
2020-10-19 14:59:37 +0200

    tusFrases 2.0

--- Assets/Scripts/SendTusFrases.cs
+++ Assets/Scripts/SendTusFrases.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class SendTusFrases : MonoBehaviour
 {
     public GameObject username;
     public GameObject userfrase;
+    public GameObject thanku;
 
     private string Name;
     private string Frase;
 
+    private TMPro.TextMeshProUGUI thankuText;
+
     [SerializeField]
     private string BASE_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSd0NYr5Ek7KZrz2uaFi0qfcSQshyc5gCax3EXIaUb_8n-hMzg/formResponse";
+
+    private void Start()
+    {
+       thankuText = thanku.GetComponent<TMPro.TextMeshProUGUI>();
+        thankuText.enabled = false;
+    }
+
 
     IEnumerator Post(string name, string frase)
     {
@@ -31,7 +42,11 @@ public class SendTusFrases : MonoBehaviour
         Name = username.GetComponent<InputField>().text;
         Frase = userfrase.GetComponent<InputField>().text;
 
-        StartCoroutine(Post(Name, Frase));
+        if (Name != "" && Frase != "")
+        {
+            StartCoroutine(Post(Name, Frase));
+            thankuText.enabled = true;
+        }
 
     }
 
